941463e1de1c38ea84b8fd3d3b67788d6dfe913d
protaisM <m.protais@proton.me>
2025-01-06 15:30:05 +0100

    find the tab bug

--- init.lua
+++ init.lua
@@ -6,10 +6,10 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
-require 'options'
-require 'keymaps'
 require 'lazy-bootstrap'
 require 'lazy-plugins'
+require 'options'
+require 'keymaps'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/options.lua
+++ lua/options.lua
@@ -10,6 +10,9 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- use a big cursor
+vim.opt.guicursor = "n-v-c-i:block"
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
@@ -20,10 +23,11 @@ end)
 
 -- Enable break indent
 vim.opt.breakindent = true
-vim.opt.autoindent=true
 vim.opt.shiftwidth=4
 vim.opt.tabstop=4
+vim.opt.softtabstop=0
 vim.opt.expandtab=true
+--vim.opt.cindent=true
 
 -- Save undo history
 vim.opt.undofile = true
